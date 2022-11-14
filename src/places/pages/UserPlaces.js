c8667e2032664733274728626efea7095ade2d4d
gla80212 <83136808+gla80212@users.noreply.github.com>
2022-11-14 08:21:13 -0500

    Update UserPlaces.js

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -3,38 +3,40 @@ import { useParams } from 'react-router-dom';
 
 import PlaceList from '../components/PlaceList';
 
-const DUMMY_PLACES = [
+const RESOURCES = [
   {
     id: 'p1',
-    title: 'University of Georgia',
-    description: 'From Ideas to Innovation!',
+    title: 'Food Bank of Northeast Georgia',
+    description: 'We envision a community in which no neighbor experiences hunger.',
     imageUrl:
-      'https://assets.simpleviewinc.com/simpleview/image/fetch/c_limit,q_75,w_1200/https://assets.simpleviewinc.com/simpleview/image/upload/crm/athens/uga-arch-large-web0-cac931605056a36_cac93607-5056-a36a-0af166f0d7ed4058.jpg',
-    address: 'University of Georgia, Athens, GA 30602',
+      'https://foodbanknega.org/wp-content/uploads/2022/02/main-logo-1.png',
+    address: '861 Newton Bridge Rd Athens, GA 30607',
     location: {
-      lat: 40.7484405,
-      lng: -73.9878584
+      lat: 34.0024821,
+      lng: -83.4013562
     },
+    link: 'https://foodbanknega.org/',
     creator: 'u1'
   },
   {
     id: 'p2',
-    title: 'Emp. State Building',
-    description: 'One of the most famous sky scrapers in the world!',
+    title: 'Athens Area Emergency Food Bank',
+    description: 'Serving our Community Since 1980',
     imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-    address: '20 W 34th St, New York, NY 10001',
+      'https://images.squarespace-cdn.com/content/v1/5555fc00e4b0e3eb2ced1494/1544108035891-71C0XP0I804ZJA9TQDOP/emergency+food+bank.jpeg?format=300w',
+    address: '440 Barber Street Athens, Georgia 30601',
     location: {
-      lat: 40.7484405,
-      lng: -73.9878584
+      lat: 33.9688268,
+      lng: -83.3863892
     },
-    creator: 'u2'
+    link: 'http://athensfoodbank.org/',
+    creator: 'u1'
   }
 ];
 
 const UserPlaces = () => {
   const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+  const loadedPlaces = RESOURCES.filter(place => place.creator === userId);
   return <PlaceList items={loadedPlaces} />;
 };
 
