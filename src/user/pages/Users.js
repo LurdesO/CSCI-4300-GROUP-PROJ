79c76678b0f0f00150d5dc1492dbf3997bb16b64
BriaButler <95509937+BriaButler@users.noreply.github.com>
2022-11-13 23:20:46 -0500

    Update Users.js

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -6,17 +6,30 @@ const Users = () => {
   const USERS = [
     {
       id: 'u1',
-      name: 'Buzz Lightyear',
+      name: 'Athens Area Emergency Food Bank',
       image:
-        'https://scontent-atl3-2.xx.fbcdn.net/v/t1.6435-9/58675136_10156465562673020_8357413523680133120_n.png?_nc_cat=104&ccb=1-7&_nc_sid=973b4a&_nc_ohc=_Ve8rbOv39QAX-0Inm8&_nc_ht=scontent-atl3-2.xx&oh=00_AfDYuQmOG-0r_Wiupevlk3G-f2EboRWSdk3hJmIRN9FsJQ&oe=638A9AA6',
-      places: 3
+      'diet.png',
     },
+
+    {
+      id: 'u1',
+      name: 'The Food Bank of Northeast Georgia',
+      image:
+      'plate.png',
+    },
+
     {
       id: 'u2',
-      name: 'Woody',
+      name: 'Little Free Pantry',
       image:
-        'https://scontent-atl3-2.xx.fbcdn.net/v/t1.6435-9/58442613_10157322613570555_1070693750134538240_n.png?_nc_cat=101&ccb=1-7&_nc_sid=973b4a&_nc_ohc=vlbC9Lkkfe4AX8STZDM&_nc_ht=scontent-atl3-2.xx&oh=00_AfAgrKFyXljfP_kMkQnizMuqlfl-luz4xTB8g1Dr1xRqWQ&oe=638A885E',
-      places: 1
+      'dish.png',
+    },
+
+    {
+      id: 'u2',
+      name: 'Ebenezer Baptist Church',
+      image:
+      'vegetables.png',
     }
   ];
 
