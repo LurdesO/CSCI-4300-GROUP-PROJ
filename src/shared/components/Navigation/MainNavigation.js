a7a00b54843b01c4f07d76c58546facaf6499119
gla80212 <83136808+gla80212@users.noreply.github.com>
2022-11-14 08:14:53 -0500

    Update MainNavigation.js

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -11,7 +11,7 @@ const MainNavigation = props => {
     <React.Fragment>
       <MainHeader>
         <h1 className="main-navigation__title">
-          <Link to="/">My Places</Link>
+          <Link to="/">Helping Hands</Link>
         </h1>
         <nav className="main-navigation__header-nav">
           <NavLinks />
