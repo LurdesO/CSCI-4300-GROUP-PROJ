2d78602840115a42a9377b10750bb67d5495a709
BriaButler <95509937+BriaButler@users.noreply.github.com>
2022-11-14 20:13:00 -0500

    Update Users.js

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -4,33 +4,12 @@ import UsersList from '../components/UsersList';
 
 const Users = () => {
   const USERS = [
-    {
+     {
       id: 'u1',
-      name: 'Athens Area Emergency Food Bank',
+      name: 'Local Food Banks',
       image:
       'diet.png',
     },
-
-    {
-      id: 'u1',
-      name: 'The Food Bank of Northeast Georgia',
-      image:
-      'plate.png',
-    },
-
-    {
-      id: 'u2',
-      name: 'Little Free Pantry',
-      image:
-      'dish.png',
-    },
-
-    {
-      id: 'u2',
-      name: 'Ebenezer Baptist Church',
-      image:
-      'vegetables.png',
-    }
   ];
 
   return <UsersList items={USERS} />;
