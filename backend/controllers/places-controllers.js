77ba34fee32b3de23175119c037a714af75a481d
LurdesO <113873412+LurdesO@users.noreply.github.com>
2022-11-28 15:51:55 -0500

    backend stuff sans the authentification

---
+++ backend/controllers/places-controllers.js
@@ -0,0 +1,191 @@
+const uuid = require('uuid/v4');
+const { validationResult } = require('express-validator');
+
+const HttpError = require('../models/http-error');
+const Place = require('../models/place');
+
+const RESOURCES = [
+  {
+    id: 'p1',
+    title: 'Food Bank of Northeast Georgia',
+    description: 'We envision a community in which no neighbor experiences hunger.',
+    imageUrl:
+      'https://foodbanknega.org/wp-content/uploads/2022/02/main-logo-1.png',
+    address: '861 Newton Bridge Rd Athens, GA 30607',
+    location: {
+      lat: 34.0024821,
+      lng: -83.4013562
+    },
+    link: 'https://foodbanknega.org/',
+    creator: 'u1'
+  },
+  {
+    id: 'p2',
+    title: 'Athens Area Emergency Food Bank',
+    description: 'Serving our Community Since 1980',
+    imageUrl:
+      'https://images.squarespace-cdn.com/content/v1/5555fc00e4b0e3eb2ced1494/1544108035891-71C0XP0I804ZJA9TQDOP/emergency+food+bank.jpeg?format=300w',
+    address: '440 Barber Street Athens, Georgia 30601',
+    location: {
+      lat: 33.9688268,
+      lng: -83.3863892
+    },
+    link: 'http://athensfoodbank.org/',
+    creator: 'u1'
+  }
+];
+
+
+const getPlaceById = async (req, res, next) => {
+  const placeId = req.params.pid; // { pid: 'p1' }
+  // const place = DUMMY_PLACES.find(p => {
+  //   return p.id === placeId;
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong, could not find a place.',
+      500
+    );
+    return next(error);
+  }
+
+  if (!place) {
+    const error = new HttpError(
+      'Could not find a place for the provided id.',
+      404
+    );
+    return next(error);
+  }
+
+  res.json({ place: place.toObject({ getters: true }) }); // => { place } => { place: place }
+};
+
+// function getPlaceById() { ... }
+// const getPlaceById = function() { ... }
+
+const getPlacesByUserId = async (req, res, next) => {
+  const userId = req.params.uid;
+
+  let places;
+  try {
+    places = await Place.find({ creator: userId });
+  } catch (err) {
+    const error = new HttpError(
+      'Fetching places failed, please try again later',
+      500
+    );
+    return next(error);
+  }
+
+  if (!places || places.length === 0) {
+    return next(
+      new HttpError('Could not find places for the provided user id.', 404)
+    );
+  }
+
+  res.json({ places: places.map(place => place.toObject({ getters: true })) });
+};
+
+const createPlace = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(
+      new HttpError('Invalid inputs passed, please check your data.', 422)
+    );
+  }
+
+  const { title, description, address, creator } = req.body;
+
+  // const title = req.body.title;
+  const createdPlace = new Place({
+    title,
+    description,
+    address,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/400px-Empire_State_Building_%28aerial_view%29.jpg',
+    creator
+  });
+
+  try {
+    await createdPlace.save(); // save the new Place in the db; save creates the unique place id
+  } catch (err) {
+    const error = new HttpError(
+      'Creating place failed, please try again.',
+      500
+    );
+    return next(error);
+  }
+
+  res.status(201).json({ place: createdPlace });
+};
+
+const updatePlace = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new HttpError('Invalid inputs passed, please check your data.', 422);
+  }
+
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong, could not update place.',
+      500
+    );
+    return next(error);
+  }
+
+  place.title = title;
+  place.description = description;
+
+  try {
+    await place.save();
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong, could not update place.',
+      500
+    );
+    return next(error);
+  }
+
+  res.status(200).json({ place: place.toObject({ getters: true }) });
+};
+
+const deletePlace = async (req, res, next) => {
+  const placeId = req.params.pid;
+
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong, could not delete place.',
+      500
+    );
+    return next(error);
+  }
+
+  try {
+    await place.remove();
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong, could not delete place.',
+      500
+    );
+    return next(error);
+  }
+
+  res.status(200).json({ message: 'Deleted place.' });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlacesByUserId = getPlacesByUserId;
+exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
